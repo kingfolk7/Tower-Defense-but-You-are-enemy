bc7cecc3e90e889a630818b75512dbb8bf240ca1
FOLK <70421993+kingfolk7@users.noreply.github.com>
2022-11-02 15:41:57 +0700

    UI & Sounds

---
+++ Tower Defense but You are enemy/Assets/Scritps/MenuController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    //private int nextLevel;
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenu;
+
+    private void Start()
+    {
+        //nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+    public void nextScene()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        SceneManager.LoadScene(1);
+    }
+    public void Quit()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+        Application.Quit();
+    }
+
+    public void toMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Escape))
+    //    {
+    //        if (gameIsPaused)
+    //        {
+    //            Resume();
+    //        }
+    //        else
+    //        {
+    //            Pause();
+    //        }
+    //    }
+    //}
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+}
